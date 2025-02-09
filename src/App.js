5dcc3d1cdc26fa0d2c0d73b847d507fae616bcad
cameronfenton <fentoncameron@gmail.com>
2025-02-09 01:23:35 -0500

    Temporarily adding a fetch for all media from the backend API for testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_GO_API_URL;
+    console.log('API URL:', apiUrl);
+
+    fetch(`${apiUrl}/media`)
+      .then((response) => {
+        console.log('Response:', response);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Data:', data);
+        setData(data);
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Record Collection Frontend</h1>
+    <div>
+      <h1>Record Collection</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
